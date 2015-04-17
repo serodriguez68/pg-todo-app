0ce4da128e13bd4414e2694b80bc0de20e96ddf3
Sergio Rodriguez <se.rodriguez68@gmail.com>
2015-04-17 12:00:11 -0500

    refactor items controller to include before_action filter

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,23 +1,20 @@
 class ItemsController < ApplicationController
 
+    before_action :find_list_and_item, only: [:edit, :update, :destroy]
+
     def edit
         Rails.logger.debug("-"*30)
         Rails.logger.debug("Params tiene: #{params}")
         Rails.logger.debug("-"*30)
-        @list = List.find(params[:list_id])
-        @item = Item.find(params[:id])
     end
 
     def update
-        @list = List.find(params[:list_id])
-        @item = Item.find(params.require(:id))
         if @item.update(secure_params)
             flash[:success] = "Item Updated"
             redirect_to list_path(@list)
         else
             render :edit
         end
-
     end
 
     def new
@@ -45,8 +42,6 @@ class ItemsController < ApplicationController
     end
 
     def destroy
-        @list = List.find(params[:list_id])
-        @item = Item.find(params.require(:id))
         @item.destroy
         flash[:success] = "Item deleted"
         redirect_to list_path(@list)
@@ -54,7 +49,14 @@ class ItemsController < ApplicationController
 
     private
 
+    def find_list_and_item
+        @list = List.find(params[:list_id])
+        @item = Item.find(params.require(:id))
+    end
+
     def secure_params
         params.require(:item).permit(:content)
     end
+
+
 end
